b166f13c73d7c3121643afaa2069e564b80c4591
david reid <zathrasorama@gmail.com>
2012-09-08 09:49:56 +0100

    Remove a level of directory to make installation easier.

---
+++ controller.php
@@ -0,0 +1,38 @@
+<?php
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class C5LinkAttributePackage extends Package {
+
+    protected $pkgHandle = 'c5_link_attribute';
+    protected $appVersionRequired = '5.5.1';
+    protected $pkgVersion = '0.1';
+
+    public function getPackageDescription() 
+    {
+         return t("An attribute type for an URL with title.");
+    }
+
+    public function getPackageName() 
+    {
+         return t("Link Attribute");
+    }
+         
+    public function install() 
+    {
+        Loader::model('attribute/type');
+        Loader::model('attribute/category');
+        
+        $pkg = parent::install();
+
+        $at = AttributeType::add('link', 'Link', $pkg);
+        $cat = AttributeKeyCategory::getByHandle('collection');
+        $cat->associateAttributeKeyType($at);
+    }
+
+    public function upgrade() 
+    {
+        $pkg = parent::install();
+    }
+    
+}
+?>
